5ef674e07b8c26f8d0d5bd1334343e7668bab266
AlexRedchets <alexey.redchets@gmail.com>
2016-07-23 17:34:58 -0400

    mvp_project

--- app/src/main/java/com/example/azvk/mvp_2/dependency_injection/ApiComponent.java
+++ app/src/main/java/com/example/azvk/mvp_2/dependency_injection/ApiComponent.java
@@ -1,7 +1,13 @@
 package com.example.azvk.mvp_2.dependency_injection;
 
+import com.example.azvk.mvp_2.MainActivity;
+
 import dagger.Component;
 
-@Component
+@CustomScope
+@Component(modules = ApiModule.class, dependencies = NetworkComponent.class)
 public interface ApiComponent {
+
+    void inject(MainActivity activity);
+
 }
--- app/src/main/java/com/example/azvk/mvp_2/dependency_injection/NetworkComponent.java
+++ app/src/main/java/com/example/azvk/mvp_2/dependency_injection/NetworkComponent.java
@@ -1,7 +1,9 @@
 package com.example.azvk.mvp_2.dependency_injection;
 
 import dagger.Component;
+import retrofit2.Retrofit;
 
 @Component(modules = NetworkModule.class)
 public interface NetworkComponent {
+    Retrofit retrofit();
 }
