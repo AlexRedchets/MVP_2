5eb5cd6ecd472acb9aee390c19551377843a128f
AlexRedchets <alexey.redchets@gmail.com>
2016-07-25 14:01:39 -0400

    done

--- app/src/main/java/com/example/azvk/mvp_2/model/PlayerAdapter.java
+++ app/src/main/java/com/example/azvk/mvp_2/model/PlayerAdapter.java
@@ -4,13 +4,16 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.azvk.mvp_2.R;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class PlayerAdapter extends RecyclerView.Adapter<PlayerAdapter.Holder> {
+public class PlayerAdapter extends RecyclerView.Adapter<PlayerAdapter.ViewHolder> {
 
     private final LayoutInflater inflater;
     private List<Player> playersList;
@@ -21,13 +24,16 @@ public class PlayerAdapter extends RecyclerView.Adapter<PlayerAdapter.Holder> {
     }
 
     @Override
-    public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return new Holder(inflater.inflate(R.layout.custom_row, parent ,false));
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ViewHolder(inflater.inflate(R.layout.custom_row, parent ,false));
     }
 
     @Override
-    public void onBindViewHolder(Holder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Player currentPlayerData = playersList.get(position);
 
+        holder.playerName.setText(currentPlayerData.getName());
+        Picasso.with(inflater.getContext()).load(currentPlayerData.getImageUrl()).into(holder.playerImage);
     }
 
     @Override
@@ -40,11 +46,16 @@ public class PlayerAdapter extends RecyclerView.Adapter<PlayerAdapter.Holder> {
         notifyDataSetChanged();
     }
 
-    public class Holder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder{
 
-        public Holder(View itemView) {
+        TextView playerName;
+        ImageView playerImage;
+
+        private ViewHolder(View itemView) {
             super(itemView);
+
+            playerImage = (ImageView)itemView.findViewById(R.id.playerImage);
+            playerName = (TextView)itemView.findViewById(R.id.playerName);
         }
     }
-
 }
